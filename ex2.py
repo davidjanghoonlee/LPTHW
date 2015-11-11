34bbacb3cbda5b8f7e8493c4f7eebbe0ec4e5cd7
David Lee <davidlee@seoulchristian.org>
2015-11-12 00:18:43 +0900

    LPTHW Ex.2

---
+++ ex2.py
@@ -0,0 +1,7 @@
+#Let me try creating a budget list for Christmas
+#the first item
+print " #1. Christmas tree: $100"
+#the second item
+print " #2. Christmas gift (Toy Car) : $30"
+#the third item
+print " #3. Christmas supper ingredients: $130"
