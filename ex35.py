cfcd1a5a50035d61f7e4743a2ec9b3458d052f1b
David Lee <davidlee@seoulchristian.org>
2016-01-14 15:48:26 +0900

    Fix ex35 mistake

--- ex35.py
+++ ex35.py
@@ -3,17 +3,16 @@ from sys import exit
 def gold_room():
     print "This room is full of gold.  How much do you take?"
 
-    choice = raw_input("> ")
-    if "0" in choice or "1" in choice:
-        how_much = int(choice)
-    else:
-        dead("Man, learn to type a number.")
+    how_much = float(raw_input("> "))
 
     if how_much < 50:
         print "Nice, you're not greedy, you win!"
         exit(0)
-    else:
+    elif how_much > 50:
         dead("You greedy bastard!")
+    else:
+        print "Men, learn to type a number!"
+        gold_room()
 
 
 def bear_room():
@@ -31,8 +30,6 @@ def bear_room():
         elif choice == "taunt bear" and not bear_moved:
             print "The bear has moved from the door. You can go through it now."
             bear_moved = True
-        elif choice == "taunt bear" and bear_moved:
-            dead("The bear gets pissed off and chews your leg off.")
         elif choice == "open door" and bear_moved:
             gold_room()
         else:
